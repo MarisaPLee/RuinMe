99c4a5598949338c9ebabd189e6e7570d7ebf3be
david.dawnkaski <dawnkd@rpi.edu>
2019-03-16 14:49:33 -0400

    ui done

--- app/src/main/java/com/example/ruinme/MainActivity.java
+++ app/src/main/java/com/example/ruinme/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ruinme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button butt = findViewById(R.id.ruinme_button);
+        butt.setTag(0);
+        butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int presses = (Integer) v.getTag();
+                v.setTag(++presses);
+                butt.setText(String.format(Locale.getDefault(), "ruin me %d", presses));
+            }
+        });
     }
 }

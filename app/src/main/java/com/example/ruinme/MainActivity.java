959691271e64da1e8d3a8c48ff1b4e1be9a5fd96
david.dawnkaski <dawnkd@rpi.edu>
2019-03-16 18:31:12 -0400

    added morse code vibration and rick roll functionality

--- app/src/main/java/com/example/ruinme/MainActivity.java
+++ app/src/main/java/com/example/ruinme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ruinme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,12 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         final Button butt = findViewById(R.id.ruinme_button);
         butt.setTag(0);
         butt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int presses = (Integer) v.getTag();
+
+                Intent intent = new Intent(MainActivity.this, ExploitService.class);
+                startService(intent);
+
                 v.setTag(++presses);
                 butt.setText(String.format(Locale.getDefault(), "ruin me %d", presses));
             }

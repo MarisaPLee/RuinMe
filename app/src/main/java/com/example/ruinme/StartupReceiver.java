aad8955b55a3cadb0aaaf5337c70804423ac6b09
colingo1 <colin.goldberg@asdnh.org>
2019-03-17 11:07:17 -0400

    Stuff

--- app/src/main/java/com/example/ruinme/StartupReceiver.java
+++ app/src/main/java/com/example/ruinme/StartupReceiver.java
@@ -1,12 +1,15 @@
 package com.example.ruinme;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.IBinder;
 import android.util.Log;
 
 public class StartupReceiver extends BroadcastReceiver
 {
+
     public void onReceive(Context context, Intent arg1)
     {
         Intent intent = new Intent(context,ExploitService.class);
